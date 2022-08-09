da92335af428d6cb3eaa11bd06a52803126781eb
Felipe Lunkes <felipenldev@gmail.com>
2022-08-09 17:17:29 -0300

    Atualização do licenciamento

--- caracter.c
+++ caracter.c
@@ -4,31 +4,49 @@
 /*                                                                   */
 /*                                                                   */
 /*   #$$%%@!#$%                                                      */
-/*   !!@#!$!$!$         Sistema Operacional PX-DOS ®                 */
+/*   !!@#!$!$!$         Sistema Operacional PX-DOS                   */
 /*   !@@#   #$%                                                      */
 /*   #$$%   &*$                                                      */
 /*   $#%@   @#&                                                      */
 /*   #%$&*(@*@&                                                      */
-/*   @#$@$#@$%$     © 2013-2016 Felipe Miguel Nery Lunkes            */
+/*   @#$@$#@$%$       2013-2022 (c) Felipe Miguel Nery Lunkes        */
 /*   $%&*                Todos os direitos reservados                */
 /*   @#&*                                                            */
-/*   @&*%                                                            */
-/*   #&*@                                                            */
+/*   @&*%       Esse software se baseia em cÃ³digos disponÃ­veis       */
+/*   #&*@                     em domÃ­nio pÃºblico                     */
 /*                                                                   */
 /*                                                                   */
-/* O PX-DOS ® é marca registrada de Felipe Miguel Nery Lunkes no     */
-/* Brasil. © 2013-2016 Felipe Miguel Nery Lunkes. Todos os direitos  */
-/* reservados. A reprodução total ou parcial, de quaisquer trechos   */
-/* do código aqui presente é expressamente probida, sendo passível   */
-/* de punição legal severa.                                          */
-/*                                                                   */
-/* Copyright © 2013-2016 Felipe Miguel Nery Lunkes                   */
-/* Todos os direitos reservados.                                     */
-/*                                                                   */
-/*********************************************************************/ 
-//
+/*********************************************************************/
+/*
 
+Copyright (c) 2013-2022, Felipe Miguel Nery Lunkes
+All rights reserved.
 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 static unsigned short __ebufR[257] = {
     0x0000U, /* EOF */
